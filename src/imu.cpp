ad1cc41adfc3926e5cf962c0efc126a5901d6cf3
KaiserKatze <donizyo@gmail.com>
2026-04-07 10:27:51 +0800

    Refactor

--- src/imu.cpp
+++ src/imu.cpp
@@ -34,6 +34,7 @@
 #include <boost/numeric/odeint.hpp>
 
 #include "euroc_vio/ahrs.hpp"
+#include "euroc_vio/rcfilter.hpp"
 
 using namespace std::chrono_literals;
 
@@ -255,41 +256,6 @@ struct ImuDerivative : public std::array<double, 10>
   }
 };
 
-/*****************************
- * RC 低通滤波器类
- *****************************/
-class RCFilter
-{
-private:
-  double alpha_;
-  bool initialized_;
-  cv::Vec3d prev_val_;
-
-public:
-  RCFilter(double alpha = 0.1)
-      : alpha_(alpha), initialized_(false), prev_val_(0, 0, 0)
-  {
-  }
-
-  cv::Vec3d Filter(const cv::Vec3d &val)
-  {
-    if (!initialized_)
-    {
-      prev_val_    = val;
-      initialized_ = true;
-      return val;
-    }
-    // 一阶低通滤波公式: Y_n = alpha * X_n + (1 - alpha) * Y_{n-1}
-    prev_val_ = alpha_ * val + (1.0 - alpha_) * prev_val_;
-    return prev_val_;
-  }
-
-  void Reset()
-  {
-    initialized_ = false;
-    prev_val_    = cv::Vec3d(0, 0, 0);
-  }
-};
 
 /*****************************
  * IMU 运动学 ODE (常微分方程) 系统
