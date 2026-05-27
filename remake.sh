628d85d80a740feff3be64f1744cd39673a3d87c
KaiserKatze <donizyo@gmail.com>
2026-05-27 16:56:25 +0800

    Refactor

--- remake.sh
+++ remake.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -eux
+
+echo "Home=$HOME"
 rm -rf build log install
 colcon build --packages-select euroc_vio \
-  --cmake-args -DCMAKE_COMMAND=/home/kk/cmake-4.3.2-linux-x86_64/bin/cmake \
+  --cmake-args -DCMAKE_COMMAND=$HOME/cmake-4.3.2-linux-x86_64/bin/cmake \
   --event-handlers console_direct+
