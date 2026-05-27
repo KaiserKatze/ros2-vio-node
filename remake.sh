df5b5a592d7bf1a9fbc8ad15434fc151be4ee803
KaiserKatze <donizyo@gmail.com>
2026-05-28 05:46:53 +0800

    Update

--- remake.sh
+++ remake.sh
@@ -4,6 +4,10 @@ set -eux
 
 echo "Home=$HOME"
 rm -rf build log install
+
+# 获取 compile_commands.json
+# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
+
 colcon build --packages-select euroc_vio \
   --cmake-args -DCMAKE_COMMAND=$HOME/cmake-4.3.2-linux-x86_64/bin/cmake \
   --event-handlers console_direct+
--- src/VisualSim/VisualInertial.cpp
+++ src/VisualSim/VisualInertial.cpp
@@ -32,6 +32,7 @@ using namespace std::chrono_literals;
 
 #include "LinearKalmanFilter.hpp"
 #include "euroc_vio/AbstractLoader.hpp"
+#include "euroc_vio/Interpolation.hpp"
 #include "euroc_vio/main.h"
 #include "zupt.hpp"
 
@@ -41,83 +42,6 @@ using namespace std::chrono_literals;
 #define DATASOURCE_EUROC 0x01
 #define DATASOURCE_SIM 0x10
 #define DATASOURCE DATASOURCE_SIM
-
-// 辅助工具：判断一个反射类型是否是特定的 Eigen 类型
-template <typename T> consteval bool is_type_of(std::meta::info member_reflect)
-{
-  return std::meta::type_of(member_reflect) == ^^T;
-}
-
-// 插值查找时间戳最近的数据（C++26 反射全自动化版）
-template <typename DataType>
-static DataType Interpolate(const std::vector<DataType> &data,
-                            const std::int64_t timestamp)
-{
-  if (data.empty())
-  {
-    throw std::runtime_error("数据为空!");
-  }
-  if (timestamp <= data.front().timestamp_)
-  {
-    return data.front();
-  }
-  if (timestamp >= data.back().timestamp_)
-  {
-    return data.back();
-  }
-
-  // 标准二分查找定位左右区间
-  size_t left{0}, right{data.size() - 1};
-  while (left + 1 < right)
-  {
-    size_t mid{(left + right) / 2};
-    if (data[mid].timestamp_ < timestamp)
-    {
-      left = mid;
-    }
-    else
-    {
-      right = mid;
-    }
-  }
-
-  const auto &datum0{data[left]};
-  const auto &datum1{data[right]};
-  const double t0 = static_cast<double>(datum0.timestamp_);
-  const double t1 = static_cast<double>(datum1.timestamp_);
-  const double alpha
-      = (t1 > t0)
-            ? std::clamp((static_cast<double>(timestamp) - t0) / (t1 - t0), 0.0,
-                         1.0)
-            : 0.0;
-
-  DataType interp;
-  template for (constexpr auto /* std::meta::info */ member :
-                std::define_static_array(std::meta::nonstatic_data_members_of(
-                    ^^DataType, std::meta::access_context::current())))
-  {
-    // 如果成员是时间戳本身，直接赋值为目标时间戳
-    if constexpr (std::meta::identifier_of(member) == "timestamp_")
-    {
-      interp.[:member:] = timestamp;
-    }
-    // 如果成员是 Eigen::Vector3f，执行线性插值 (LERP)
-    else if constexpr (is_type_of<Eigen::Vector3f>(member))
-    {
-      interp.[:member:] = datum0.[:member:]
-          * (1.0f - static_cast<float>(alpha)) + datum1.[:member:]
-          * static_cast<float>(alpha);
-    }
-    // 如果成员是 Eigen::Quaternionf，执行球面线性插值 (SLERP)
-    else if constexpr (is_type_of<Eigen::Quaternionf>(member))
-    {
-      interp.[:member:] = datum0.[:member:].slerp(static_cast<float>(alpha),
-                                                  datum1.[:member:]);
-    }
-  }
-
-  return interp;
-}
 
 struct DatumFast
 {
