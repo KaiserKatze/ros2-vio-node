894c4eac1b7c09aedd5d2e960e0af52ff0b9023a
KaiserKatze <donizyo@gmail.com>
2026-06-06 08:43:19 +0800

    Update

--- src/VisualSlam/Eigen.hpp
+++ src/VisualSlam/Eigen.hpp
@@ -4,6 +4,13 @@
 
 namespace Eigen
 {
+using Vector6f    = Eigen::Matrix<float, 6, 1>;
+using RowVector6f = Eigen::Matrix<float, 1, 6>;
+using MatrixX6f   = Eigen::Matrix<float, Eigen::Dynamic, 6>;
+using Vector9f    = Eigen::Matrix<float, 9, 1>;
+using MatrixX9f   = Eigen::Matrix<float, Eigen::Dynamic, 9>;
+using Matrix34f   = Eigen::Matrix<float, 3, 4>;
+
 using Vector6d    = Eigen::Matrix<double, 6, 1>;
 using RowVector6d = Eigen::Matrix<double, 1, 6>;
 using MatrixX6d   = Eigen::Matrix<double, Eigen::Dynamic, 6>;
