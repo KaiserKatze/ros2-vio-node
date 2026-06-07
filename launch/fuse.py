96aedfe3406a1474e65b7c10985a541c274ecd3f
KaiserKatze <donizyo@gmail.com>
2026-06-07 16:38:59 +0800

    Update

--- launch/fuse.py
+++ launch/fuse.py
@@ -16,36 +16,30 @@ def generate_launch_description():
     home_path = pathlib.PosixPath(os.path.expanduser("~"))
 
     # mav0_path = home_path / "vio_ws" / "mav0"
-    mav0_path = pathlib.PosixPath("/mnt/e/Documents/mav0")
+    mav0_path = pathlib.PosixPath("/mnt", "e", "Documents", "mav0")
 
     cam0_path = mav0_path / "cam0"
     imu0_path = mav0_path / "imu0"
     truth_path = mav0_path / "state_groundtruth_estimate0"
 
-    # 输出: 轨迹话题 /path_fast_est
-    # 输出: 轨迹话题 /pose_fast_est
-    # 输出: 轨迹话题 /path_fuse_est
-    # 输出: 轨迹话题 /pose_fuse_est
+    params = {
+        "use_evo_sim3": False,
+        "use_true_translation_in_fast": True,
+        "use_true_init_pose": True,
+        "path_estimation_csv": str(home_path / "vio_ws" / "estimated_motion.csv"),
+        "path_imu_csv": str(imu0_path / "data.csv"),
+        "path_imu_yaml": str(imu0_path / "sensor.yaml"),
+        "path_truth_csv": str(truth_path / "data.csv"),
+        "path_cam0_yaml": str(cam0_path / "sensor.yaml"),
+        "path_truth_yaml": str(truth_path / "sensor.yaml"),
+    }
+
     fuse_node = Node(
         package="euroc_vio",
         executable="VisualInertial",
         name="VisualInertial",
         output="screen",
-        parameters=[
-            {
-                "use_evo_sim3": False,
-                "use_true_translation_in_fast": True,
-                "use_true_init_pose": True,
-                "path_estimation_csv": str(
-                    home_path / "vio_ws" / "estimated_motion.csv"
-                ),
-                "path_imu_csv": str(imu0_path / "data.csv"),
-                "path_imu_yaml": str(imu0_path / "sensor.yaml"),
-                "path_truth_csv": str(truth_path / "data.csv"),
-                "path_cam0_yaml": str(cam0_path / "sensor.yaml"),
-                "path_truth_yaml": str(truth_path / "sensor.yaml"),
-            }
-        ],
+        parameters=[params],
     )
 
     rviz_node = Node(
