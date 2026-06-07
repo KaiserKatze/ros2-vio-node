4a54d0855885391cf79ed562427b6903c824b463
KaiserKatze <donizyo@gmail.com>
2026-06-07 22:24:07 +0800

    Update

--- remake.sh
+++ remake.sh
@@ -3,13 +3,19 @@
 set -eux
 
 echo "Home=$HOME"
-rm -rf build log install
-
-# 获取 compile_commands.json
-# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .
 
 colcon build --packages-select euroc_vio \
   --cmake-args \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo \
     -D CMAKE_COMMAND=$HOME/cmake-4.3.2-linux-x86_64/bin/cmake \
   --event-handlers console_direct+
+
+if [ $? -ne 0 ]; then
+  rm -rf build log install
+
+  colcon build --packages-select euroc_vio \
+    --cmake-args \
+      -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+      -D CMAKE_COMMAND=$HOME/cmake-4.3.2-linux-x86_64/bin/cmake \
+    --event-handlers console_direct+
+fi
