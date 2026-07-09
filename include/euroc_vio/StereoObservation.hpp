a21023e96db779b91438fee91ce27e133f2e7fdc
KaiserKatze <donizyo@gmail.com>
2026-07-09 18:42:08 +0800

    Update

--- include/euroc_vio/StereoObservation.hpp
+++ include/euroc_vio/StereoObservation.hpp
@@ -8,6 +8,7 @@ template <typename value_type = double>
 struct StereoObservation
 {
   using Vector2 = Eigen::Vector<value_type, 2>;
+  using Vector3 = Eigen::Vector<value_type, 3>;
 
   // 路标点 ID
   std::uint32_t feature_id_{0};
@@ -15,8 +16,6 @@ struct StereoObservation
   Vector2 pt_left_{Vector2::Zero()};
   // 右目图像中角点坐标
   Vector2 pt_right_{Vector2::Zero()};
-  // 左目图像中角点响应值 (FAST score)
-  value_type response_left_{0.0};
-  // 右目图像中角点响应值 (FAST score)
-  value_type response_right_{0.0};
+  // 通过三角化得到的路标点在左目相机坐标系下的坐标
+  Vector3 landmark_{Vector3::Zero()};
 };
