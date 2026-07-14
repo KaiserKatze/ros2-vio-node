1683f333dc9c46119acf7dad624b41d69475155f
KaiserKatze <donizyo@gmail.com>
2026-07-14 21:12:16 +0800

    Update

--- remake.sh
+++ remake.sh
@@ -9,7 +9,14 @@ rm -rf build log install
 export MAKEFLAGS="-j1"
 colcon build --packages-select euroc_vio \
   --parallel-workers 1 \
-  --cmake-args \
+  --cmake-args -G Ninja \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo \
     -D CMAKE_COMMAND=$HOME/cmake-4.3.2-linux-x86_64/bin/cmake \
+  --event-handlers console_direct+
+
+rm -rf build log install
+colcon build --packages-select euroc_vio \
+  --parallel-workers 1 \
+  --cmake-args -G Ninja \
+    -D CMAKE_BUILD_TYPE=RelWithDebInfo \
   --event-handlers console_direct+
