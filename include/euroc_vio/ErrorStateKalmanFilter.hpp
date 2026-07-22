9e6146b03f1ddfe549eeadfa61b69a61e0fa31a2
KaiserKatze <donizyo@gmail.com>
2026-07-22 13:49:34 +0800

    Fix

--- include/euroc_vio/ErrorStateKalmanFilter.hpp
+++ include/euroc_vio/ErrorStateKalmanFilter.hpp
@@ -1249,6 +1249,9 @@ private:
   static auto GetKalmanGain(const TransitionMatrix &P,
                             const JacobiMeasurement &H,
                             const CovarianceMeasurement &V) noexcept
+      -> Eigen::Matrix<value_type,
+                       JacobiMeasurement::ColsAtCompileTime,
+                       JacobiMeasurement::RowsAtCompileTime>
   {
     auto hphv{H * P * H.transpose() + V};
     return hphv.ldlt().solve(H * P.transpose()).transpose();
